f6f9aeb4f501912315b8736c72fb47563e15f8d1
maidormo <elia.gatti@studenti.unitn.it>
2024-04-27 22:00:10 +0200

    Added the ability to place ships that have size > 1 (still randomly) Refactored all of the code

--- grid.py
+++ grid.py
@@ -1,61 +1,78 @@
 import random
 from constants import EMPTY, GRID_SIZE, HIT, MISS, NUM_CELLS, SHIP, DEBUG, SIZE
+from ships import Ship, ShipType
 
-#Implementation of the grid class
-"""The idea behind this class is to have the ability to store the player grid"""
 class Grid:
-
     def __init__(self, pygame, screen):
-        self.grid = [[EMPTY for x in range(NUM_CELLS)] for y in range(NUM_CELLS)]
+        self.grid = [[EMPTY for _ in range(NUM_CELLS)] for _ in range(NUM_CELLS)]
         self.pygame = pygame
         self.screen = screen
+        self.ships = [Ship(ship_type) for ship_type in ShipType]
 
     def generate_random_coordinates(self):
-        return random.randint(0, NUM_CELLS - 1), random.randint(0, NUM_CELLS - 1)
+        return random.randint(0, NUM_CELLS - 1), random.randint(0, NUM_CELLS - 1), random.choice([True,False])
+
+    def is_valid_position(self, x: int, y: int) -> bool:
+        return 0 <= x < NUM_CELLS and 0 <= y < NUM_CELLS and self.grid[x][y] != SHIP
 
     def place_ships(self):
-        # Place 5 ships
-        for _ in range(5):
-            # Randomly place the ship
-            x, y = self.generate_random_coordinates()
+        for ship in self.ships:
+            self.place_ship(ship)
 
-            # Check if the cell is already occupied by a ship
-            while self.grid[x][y] == SHIP:
-                # If the cell is occupied, generate new random coordinates
-                x, y = self.generate_random_coordinates()
+    def place_ship(self, ship):
+        while True:
+            x, y, is_horizontal = self.generate_random_coordinates()
+            if self.can_be_placed(x, y, is_horizontal, ship):
+                for i in range(ship.health):
+                    if is_horizontal:
+                        self.grid[x+i][y] = SHIP
+                    else:
+                        self.grid[x][y+i] = SHIP
+                break
 
-            # Place the ship
-            self.grid[x][y] = SHIP
-            if DEBUG:
-                print(f"Ship placed at ({chr(65 + x)}, {y + 1})")
+    def can_be_placed(self, x: int, y: int, is_horizontal: bool, ship: Ship) -> bool:
+        for i in range(ship.health):
+            if not self.is_valid_position(x+i, y) if is_horizontal else not self.is_valid_position(x, y+i):
+                return False
+        return True
+
+    def draw(self, offset):
+        start_x = (self.screen.get_width() - SIZE*offset) // 2
+        start_y = (self.screen.get_height() - SIZE) // 2
+
+        for x in range(NUM_CELLS + 1):
+            self.draw_line("white", (start_x + x * GRID_SIZE, start_y), (start_x + x * GRID_SIZE, start_y + SIZE))
+
+        for y in range(NUM_CELLS + 1):
+            self.draw_line("white", (start_x, start_y + y * GRID_SIZE), (start_x + SIZE, start_y + y * GRID_SIZE))
+
+        draw_methods = {
+            MISS: self.draw_miss,
+            HIT: self.draw_hit,
+            SHIP: self.draw_ship
+        }
+
+        for x in range(NUM_CELLS):
+            for y in range(NUM_CELLS):
+                draw_method = draw_methods.get(self.grid[x][y])
+                if draw_method:
+                    draw_method(start_x, start_y, x, y)
+
+        self.draw_labels(start_x, start_y)
 
     def draw_line(self, color, start, end):
         self.pygame.draw.line(self.screen, color, start, end)
 
-    def draw(self,offset):
-        # Calculate the starting point to center the grid
-        start_x = (self.screen.get_width() - SIZE*offset) // 2
-        start_y = (self.screen.get_height() - SIZE) // 2
+    def draw_miss(self, start_x, start_y, x, y):
+        self.pygame.draw.circle(self.screen, "blue", (start_x + x * GRID_SIZE + GRID_SIZE // 2, start_y + y * GRID_SIZE + GRID_SIZE // 2), GRID_SIZE // 4)
 
-        # Draw the vertical lines
-        for x in range(NUM_CELLS + 1):
-            self.draw_line("white", (start_x + x * GRID_SIZE, start_y), (start_x + x * GRID_SIZE, start_y + SIZE))
+    def draw_hit(self, start_x, start_y, x, y):
+        self.pygame.draw.circle(self.screen, "red", (start_x + x * GRID_SIZE + GRID_SIZE // 2, start_y + y * GRID_SIZE + GRID_SIZE // 2), GRID_SIZE // 4)
 
-        # Draw the horizontal lines
-        for y in range(NUM_CELLS + 1):
-            self.draw_line("white", (start_x, start_y + y * GRID_SIZE), (start_x + SIZE, start_y + y * GRID_SIZE))
-        
-        # Draw the hits and misses
-        for x in range(NUM_CELLS):
-            for y in range(NUM_CELLS):
-                if self.grid[x][y] == MISS:
-                    self.pygame.draw.circle(self.screen, "blue", (start_x + x * GRID_SIZE + GRID_SIZE // 2, start_y + y * GRID_SIZE + GRID_SIZE // 2), GRID_SIZE // 4)
-                elif self.grid[x][y] == HIT: 
-                    self.pygame.draw.circle(self.screen, "red", (start_x + x * GRID_SIZE + GRID_SIZE // 2, start_y + y * GRID_SIZE + GRID_SIZE // 2), GRID_SIZE // 4)
-                elif self.grid[x][y] == SHIP:
-                    self.pygame.draw.rect(self.screen, "red", (start_x + x * GRID_SIZE, start_y + y * GRID_SIZE, GRID_SIZE, GRID_SIZE))
-                
-        # Draw the labels
+    def draw_ship(self, start_x, start_y, x, y):
+        self.pygame.draw.rect(self.screen, "red", (start_x + x * GRID_SIZE, start_y + y * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+
+    def draw_labels(self, start_x, start_y):
         font = self.pygame.font.Font(None, 24)
         for x in range(NUM_CELLS):
             label = font.render(chr(65 + x), 1, (255, 255, 255))
